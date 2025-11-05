ae527ed0b987f6c3d1fce1f4b72204f943820a87
Tony <z999zon@gmail.com>
2025-11-05 23:00:32 +0530

    Add files via upload

---
+++ Sum_Ab.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Sum_Ab {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Value of A and B:");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        int sum = a + b;
+        System.out.println("Sum of A and B : " + sum);
+    }
+}
